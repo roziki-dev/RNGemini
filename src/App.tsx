e250f6301878268e26cdabf0b31664f7bc17a56a
Rozi <muh.roziki@gmail.com>
2024-07-28 16:11:19 +0700

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
-import React, { useCallback, useRef, useState } from 'react';
+import React, {useCallback, useEffect, useRef, useState} from 'react';
 
 import {
-  ActivityIndicator,
+  Dimensions,
   FlatList,
-  KeyboardAvoidingView,
-  Platform,
+  Image,
+  InteractionManager,
   SafeAreaView,
   StatusBar,
   StyleSheet,
   Text,
-  TextInput,
-  TouchableOpacity,
   View,
 } from 'react-native';
 
 import Config from 'react-native-config';
-import { GoogleGenerativeAI } from '@google/generative-ai';
+import {GoogleGenerativeAI} from '@google/generative-ai';
+import GradientText from 'react-native-gradient-texts';
+import {Footer} from './assets/components/Footer';
+import {EmptyState} from './assets/components/EmptyState';
+
+const windowWidth = Dimensions.get('window').width;
 
 const API_KEY: any | string = Config.GEMINI_AI_KEY;
+const geminiIcon = require('./assets/images/google-gemini-icon.png');
+const boyIcon = require('./assets/images/boy.png');
 
 function App() {
-  const refScroll = useRef();
+  const refScroll = useRef(null);
+  const refInput = useRef(null);
   const genAI = new GoogleGenerativeAI(API_KEY);
   const model = genAI.getGenerativeModel({
     model: 'gemini-1.5-flash',
@@ -29,55 +35,117 @@ function App() {
   const [listResult, setListResult] = useState([]);
   const [loading, setLoading] = useState(false);
 
-  const onGenerate = useCallback(async () => {
-    try {
-      const resultObj: any = {};
-      if (prompt.length === 0) {
-        return false;
+  useEffect(() => {
+    const task = InteractionManager.runAfterInteractions(() => {
+      refInput.current && refInput.current.focus();
+    });
+
+    return () => {
+      task.cancel();
+    };
+  }, []);
+
+  useEffect(() => {
+    let delay: any = null;
+    const task = InteractionManager.runAfterInteractions(() => {
+      if (listResult.length > 2) {
+        delay = setTimeout(() => {
+          refScroll.current &&
+            refScroll.current.scrollToItem({
+              animated: true,
+              item: listResult[listResult.length - 1],
+              viewPosition: 0,
+            });
+        }, 500);
       }
-      setLoading(true);
+    });
 
-      let requestObj = {
-        name: 'Anda',
-        result: prompt,
-      };
+    return () => {
+      task.cancel();
+      delay && clearTimeout(delay);
+    };
+  }, [listResult]);
 
-      setListResult(o => [...o, requestObj]);
+  const onGenerate = useCallback(() => {
+    const resultObj: any = {};
+    try {
+      requestAnimationFrame(async () => {
+        if (prompt.length === 0) {
+          return false;
+        }
+        setLoading(true);
 
-      // result from Gemini
-      const result = await model.generateContent(prompt);
-      const response = result.response;
-      const resultText: string = response.text();
-      resultObj.name = 'AI Bot';
-      resultObj.request = prompt;
-      resultObj.result = resultText;
-      console.log(prompt);
-      console.log(resultText);
-      setListResult(o => [...o, resultObj]);
-      setPromp('');
-      setLoading(false);
-      refScroll.current && refScroll.current.scrollToEnd({ animated: true });
+        let requestObj = {
+          name: 'Anda',
+          result: prompt,
+        };
+
+        setListResult(o => [...o, requestObj]);
+
+        // result from Gemini
+        const result = await model.generateContent(prompt);
+        const response = result.response;
+        const resultText: string = response.text();
+        resultObj.name = 'AI Bot';
+        resultObj.result = resultText;
+        setListResult(o => [...o, resultObj]);
+        setPromp('');
+        setLoading(false);
+      });
     } catch (error) {
       console.log('Error: ', error);
+      resultObj.name = 'AI Bot';
+      resultObj.result =
+        "I don't understand what you wrote. Please try again later!";
+      setListResult(o => [...o, resultObj]);
       setLoading(false);
     }
-  }, [model, prompt, refScroll]);
+  }, [model, prompt]);
 
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar barStyle="dark-content" />
-      <View>
-        <Text style={styles.title}>Google Gemini AI</Text>
+
+      <View style={styles.header}>
+        <GradientText
+          text={'Google Gemini AI'}
+          fontSize={20}
+          isGradientFill
+          isGradientStroke
+          width={windowWidth}
+          style={styles.title}
+          locations={{x: windowWidth / 2, y: 25}}
+          gradientColors={['#2623c2', '#d91434']}
+          fontFamily={'Montserrat Regular'}
+        />
       </View>
       <FlatList
         ref={refScroll}
         keyExtractor={(item, index) => index.toString()}
         data={listResult}
-        renderItem={({ item }) => (
-          <View style={styles.itemWrap}>
-            <View style={styles.itemImg} />
-            <View style={{ flex: 1 }}>
-              <Text style={styles.name}>{item?.name}</Text>
+        removeClippedSubviews={false}
+        renderItem={({item}) => (
+          <View
+            style={[
+              styles.itemWrap,
+              item?.name === 'Anda' && {
+                backgroundColor: 'transparent',
+              },
+            ]}>
+            <View
+              style={[
+                styles.itemImg,
+                item?.name === 'Anda' && {backgroundColor: '#ddd'},
+                item?.name === 'Anda' && {borderColor: '#bfbfbf'},
+              ]}>
+              {item?.name === 'Anda' ? (
+                <Image source={boyIcon} style={{width: 24, height: 24}} />
+              ) : (
+                <Image source={geminiIcon} style={{width: 24, height: 24}} />
+              )}
+            </View>
+            <View style={{flex: 1, marginTop: 10}}>
+              {/* <Text style={styles.name}>{item?.name}</Text> */}
               <View style={styles.result}>
                 <Text style={styles.resultText}>{item.result}</Text>
               </View>
@@ -87,35 +155,15 @@ function App() {
         style={styles.body}
         contentContainerStyle={styles.bodyInside}
         showsVerticalScrollIndicator={true}
+        ListEmptyComponent={<EmptyState />}
       />
-      <KeyboardAvoidingView
-        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
-        <View style={styles.footer}>
-          <TextInput
-            placeholder="Tuliskan disini"
-            style={styles.textInput}
-            value={prompt}
-            editable={!loading}
-            underlineColorAndroid={'transparent'}
-            placeholderTextColor={'#999'}
-            selectionColor={'#000'}
-            onChangeText={setPromp}
-            autoComplete="off"
-            autoCorrect={false}
-            autoCapitalize="none"
-          />
-          <TouchableOpacity
-            disabled={loading}
-            style={styles.button}
-            onPress={onGenerate}>
-            {!loading ? (
-              <Text style={styles.buttonText}>Generate</Text>
-            ) : (
-              <ActivityIndicator size="small" color="#fff" />
-            )}
-          </TouchableOpacity>
-        </View>
-      </KeyboardAvoidingView>
+      <Footer
+        ref={refInput}
+        value={prompt}
+        onChangeText={setPromp}
+        onSend={onGenerate}
+        loading={loading}
+      />
     </SafeAreaView>
   );
 }
@@ -123,57 +171,31 @@ function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#f5f5f5',
+    backgroundColor: '#fff',
   },
   title: {
-    fontFamily: 'Pacifico Regular',
-    textAlign: 'center',
-    fontSize: 20,
+    maxHeight: 50,
+  },
+  header: {
+    maxHeight: 50,
+    width: windowWidth,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    borderBottomColor: '#e8e8e8',
+    borderBottomWidth: 1,
   },
   body: {
     flex: 1,
-    backgroundColor: '#fff',
-    borderRadius: 16,
-    marginHorizontal: 8,
-    marginVertical: 16,
+    padding: 8,
+    marginBottom: 16,
+    backgroundColor: 'transparent',
   },
   bodyInside: {
     padding: 16,
   },
-  textInput: {
-    fontFamily: 'Montserrat Regular',
-    flex: 1,
-    backgroundColor: '#fff',
-    borderRadius: 25,
-    paddingHorizontal: 16,
-    marginHorizontal: 8,
-    paddingVertical: 12,
-  },
-  footer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    paddingBottom: 16,
-  },
-  button: {
-    backgroundColor: 'brown',
-    paddingHorizontal: 16,
-    paddingVertical: 12,
-    marginHorizontal: 8,
-    borderRadius: 25,
-  },
-  buttonText: {
-    color: '#fff',
-    fontFamily: 'Montserrat Bold',
-  },
-  result: {
-    backgroundColor: '#f7f1e4',
-    borderRadius: 16,
-    paddingHorizontal: 16,
-    paddingVertical: 12,
-    borderTopLeftRadius: 2,
-  },
+  result: {},
   resultText: {
-    fontFamily: 'Montserrat Regular',
     fontSize: 14,
     color: '#000',
     lineHeight: 20,
@@ -188,15 +210,23 @@ const styles = StyleSheet.create({
     marginBottom: 4,
   },
   itemWrap: {
+    backgroundColor: 'rgba(184, 32, 214,.05)',
+    borderRadius: 16,
     flexDirection: 'row',
     marginVertical: 10,
+    paddingHorizontal: 16,
+    paddingVertical: 12,
   },
   itemImg: {
     marginRight: 8,
     width: 42,
     height: 42,
     borderRadius: 100,
-    backgroundColor: '#f5f5f5',
+    backgroundColor: 'rgba(79, 53, 212,0.1)',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: 'rgba(184, 32, 214,0.3)',
+    borderWidth: 2,
   },
 });
 
